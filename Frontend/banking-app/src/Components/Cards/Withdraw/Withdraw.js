dc05f761c106d04dfb8549bdd89243e4db476a33
Shrinath Maruti Suryawanshi <shrinath.10823694@ltimindtree.com>
2024-11-27 12:58:01 +0530

    bg color for withdraw loader

--- Frontend/banking-app/src/Components/Cards/Withdraw/Withdraw.js
+++ Frontend/banking-app/src/Components/Cards/Withdraw/Withdraw.js
@@ -152,6 +152,7 @@ export default function Withdraw() {
             left: 0,
             width: "100%",
             height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
             zIndex: 1000,
             display: "flex",
             flexDirection: "column",
